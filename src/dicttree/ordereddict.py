f29116a9cbbf7b4e463ae1e4889f774de9eff964
Florian Bauer <florian.bauer@diasemi.com>
2011-09-07 16:01:18 +0200

    use absolute import

--- src/dicttree/ordereddict.py
+++ src/dicttree/ordereddict.py
@@ -1,7 +1,8 @@
+from __future__ import absolute_import
 try:
     from collections import OrderedDict
-except ImportError:
-    from ordereddict import OrderedDict
+except ImportError:                      #pragma NO COVERAGE
+    from ordereddict import OrderedDict  #pragma NO COVERAGE
 
 from dicttree import aspects as dta
 
