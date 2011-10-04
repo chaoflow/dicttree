a19b3ab66433fed6fd5e3d3479415d68b30c86e9
Florian Friesdorf <flo@chaoflow.net>
2011-10-04 20:07:59 +0200

    mgroup aspect support

--- src/dicttree/aspects/_views.py
+++ src/dicttree/aspects/_views.py
@@ -33,6 +33,7 @@ class mgroup(Aspect):
 
     input tree needs to be sorted
     """
+    _cfg_aspects = aspect.aspectkw(aspects=())
     # a list of functions to derive the group key for each tier
     _cfg_groupkeys = aspect.aspectkw(groupkeys=())
 
@@ -44,10 +45,14 @@ class mgroup(Aspect):
 
     @aspect.plumb
     def itervalues(_next, self):
-        if not self._cfg_groupkeys:
-            return _next()
-        gkey = self._cfg_groupkeys[0]
-        query = lambda key: lambda : filter(lambda x: gkey(x) == key, _next())
-        return (mgroup(Itervalues(name=key, itervalues=query(key)),
-                       groupkeys=self._cfg_groupkeys[1:])
-                for key, _ in groupby(_next(), gkey))
+        gener = _next()
+        if self._cfg_groupkeys:
+            gkey = self._cfg_groupkeys[0]
+            query = lambda key: lambda : filter(lambda x: gkey(x) == key, _next())
+            gener = (mgroup(Itervalues(name=key, itervalues=query(key)),
+                            groupkeys=self._cfg_groupkeys[1:],
+                            aspects=self._cfg_aspects[1:])
+                     for key, _ in groupby(gener, gkey))
+        if len(self._cfg_aspects) > 0:
+            gener = (self._cfg_aspects[0](x) for x in gener)
+        return gener
