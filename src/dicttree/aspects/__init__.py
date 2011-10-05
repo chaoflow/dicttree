80bc2cdc5b355fe6474f78df6361ba29ec967841
Florian Friesdorf <flo@chaoflow.net>
2011-10-05 07:53:19 +0200

    factored out adoptation

--- src/dicttree/aspects/__init__.py
+++ src/dicttree/aspects/__init__.py
@@ -5,6 +5,7 @@ from zope.interface import implements
 from dicttree.aspects._views import Itervalues
 from dicttree.aspects._views import leaves
 from dicttree.aspects._views import mgroup
+from dicttree.aspects.adoption import *
 from dicttree.interfaces import INode
 
 
@@ -13,54 +14,6 @@ from itertools import imap
 def name_in_mro(obj, key):
     return any(imap(lambda x: key in x.__dict__,
                     [obj] + obj.__class__.mro()))
-
-
-class adopting(Aspect):
-    @aspect.plumb
-    def __delitem__(_next, self, key):
-        # free the child for adoption
-        value = self[key]
-        value.__name__ = None
-        value.__parent__ = None
-        _next(key)
-
-    @aspect.plumb
-    def __setitem__(_next, self, key, value, skip_name_check=False):
-        if not skip_name_check and value.__name__ is not None:
-            raise ValueError("Cannot adopt child with name")
-        if value.__parent__ is not None:
-            raise ValueError("Cannot adopt child with parent")
-        # adopt
-        value.__name__ = key
-        value.__parent__ = self
-        try:
-            _next(key, value)
-        except:
-            value.__name__ = None
-            value.__parent__ = None
-            raise
-
-
-# XXX: Is zope.location worth it, i.e. small enough to be used here?
-# XXX: __name__/__parent__ vs _name/_parent
-class adoptable(Aspect):
-    __name__ = None
-    __parent__ = None
-
-    @property
-    def name(self):
-        return self.__name__
-
-    @property
-    def parent(self):
-        return self.__parent__
-
-    # XXX: *args are currently not supported, not even passing them on
-    @aspect.plumb
-    def __init__(_next, self, name=None, **kw):
-        _next(**kw)
-        if name is not None:
-            self.__name__ = name
 
 
 class appendchild(Aspect):
---
+++ src/dicttree/aspects/adoption.py
@@ -0,0 +1,52 @@
+from metachao import aspect
+from metachao.aspect import Aspect
+
+
+class adopting(Aspect):
+    @aspect.plumb
+    def __delitem__(_next, self, key):
+        # free the child for adoption
+        value = self[key]
+        value.__name__ = None
+        value.__parent__ = None
+        _next(key)
+
+    @aspect.plumb
+    def __setitem__(_next, self, key, value, skip_name_check=False):
+        if not skip_name_check and value.__name__ is not None:
+            raise ValueError("Cannot adopt child with name")
+        if value.__parent__ is not None:
+            raise ValueError("Cannot adopt child with parent")
+        # adopt
+        value.__name__ = key
+        value.__parent__ = self
+        try:
+            _next(key, value)
+        except:
+            value.__name__ = None
+            value.__parent__ = None
+            raise
+
+
+# XXX: Is zope.location worth it, i.e. small enough to be used here?
+# XXX: __name__/__parent__ vs _name/_parent
+class adoptable(Aspect):
+    __name__ = aspect.aspectkw(name=None)
+    __parent__ = aspect.aspectkw(parent=None)
+
+    @property
+    def name(self):
+        return self.__name__
+
+    @property
+    def parent(self):
+        return self.__parent__
+
+    # XXX: *args are currently not supported, not even passing them on
+    @aspect.plumb
+    def __init__(_next, self, name=None, parent=None, **kw):
+        _next(**kw)
+        if name is not None:
+            self.__name__ = name
+        if parent is not None:
+            self.__parent__ = parent
