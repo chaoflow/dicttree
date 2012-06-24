8658b585d75933a86736fc25c345f12c1a756244
Florian Friesdorf <flo@chaoflow.net>
2012-06-24 19:11:44 +0200

    fix csv test

--- src/dicttree/tests/test_csv.py
+++ src/dicttree/tests/test_csv.py
@@ -2,20 +2,20 @@ from pkg_resources import resource_stream
 
 from unittest import TestCase
 
-from dicttree.csv import csv, Column
+from dicttree.csv import csv, Field
 from dicttree.ordereddict import Node
 
 
 class CSVTest(TestCase):
     def runTest(self):
         node = Node()
-        columns = [
-            Column(name='a', type=int),
-            Column(name='b', type=str),
-            Column(name='c', type=int, default=9),
+        schema = [
+            Field(name='a', type=int),
+            Field(name='b', type=str),
+            Field(name='c', type=int, default=9),
             ]
         with resource_stream(__name__, 'test_csv.csv') as s:
-            csv(node, columns=columns, skip=1).load(s)
+            csv(node, schema=schema, skip=1).load(s)
         self.assertListEqual(node.keys(), [0,1])
         self.assertListEqual(node[0].attrs.keys(), ['a', 'b', 'c'])
         self.assertListEqual(node[1].attrs.keys(), ['a', 'b', 'c'])
